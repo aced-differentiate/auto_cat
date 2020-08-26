3b34b720d8b8144b25762e45540a5fe8c2334db7
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-08-26 12:37:41 -0400

    Start building SAA unittests

--- tests/test_saa.py
+++ tests/test_saa.py
@@ -1,9 +1,13 @@
 import unittest
 import os
+import shutil
+from autocat import gen_saa
 
 
-# gen_saa(["Cu"], ["Fe"], ft=["111"], supcell=(3, 3, 4), a=1.813471 * 2)
-# gen_saa(['Cu'],['Fe'],supcell=(5,5,4),a=1.813471*2)
+class TestSAA(unittest.TestCase):
+    def test_CuFe(self):
+        gen_saa(["Cu"], ["Fe"], supcell=(5, 5, 4), a=1.813471 * 2)
+
 
 if __name__ == "__main__":
     unittest.main()
