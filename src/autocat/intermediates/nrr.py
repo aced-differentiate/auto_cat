b2c62d23f895c255bf2915c49147abf67b57b24a
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-09-01 16:52:02 -0400

    Extends ORR and NRR int list to include even ints that will be handled by g2 and atomic symbols

--- src/autocat/intermediates/nrr.py
+++ src/autocat/intermediates/nrr.py
@@ -1,8 +1,8 @@
 from ase import Atoms
 
-nrr_intermediate_names = ["NNH", "NNH2", "NH", "NHNH", "NHNH2"]
+nrr_intermediate_names = ["NNH", "NNH2", "N", "NH", "NH2", "NHNH", "NHNH2"]
 
-
+# N is in atomic symbols and NH2 is in g2
 nrr_mols = {
     "NNH": Atoms("N2H", [(0.0, 0.0, 0.0), (-0.2, 0.0, 1.2), (0.51, 0, 1.91)]),
     "NNH2": None,
--- src/autocat/intermediates/orr.py
+++ src/autocat/intermediates/orr.py
@@ -1,7 +1,8 @@
 from ase import Atoms
 
-orr_intermediate_names = ["OOH", "OH"]
+orr_intermediate_names = ["OOH", "O", "OH"]
 
+# O is in atomic symbols
 orr_mols = {
     "OOH": Atoms("O2H", [(0.0, 0.0, 0.0), (0.0, 1.2, 0.8), (0.0, 0.8, 1.6)]),
     "OH": Atoms("OH", [(0.0, 0.0, 0.0), (0.0, 0.0, 0.97)]),
