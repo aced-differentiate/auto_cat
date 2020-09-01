8b523ab9627f2f095fb2ed21e84aa5551cbefe1b
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-09-01 13:22:22 -0400

    Adds ability to automate fixing of the bottom layers

--- src/autocat/bulk.py
+++ src/autocat/bulk.py
@@ -2,10 +2,16 @@ import os
 from ase.io import read, write
 from ase.build import fcc100, fcc110, fcc111
 from ase.build import bcc100, bcc110, bcc111
+from ase.constraints import FixAtoms
 
 
 def gen_bulk_dirs(
-    species_list, bv=["fcc"], ft=["100", "110", "111"], supcell=(3, 3, 4), a=[None]
+    species_list,
+    bv=["fcc"],
+    ft=["100", "110", "111"],
+    supcell=(3, 3, 4),
+    a=[None],
+    fix=0,
 ):
     """
     Given list of species, bravais lattice, and facets creates directories containing traj files for the surfaces
@@ -23,7 +29,7 @@ def gen_bulk_dirs(
     curr_dir = os.getcwd()
     i = 0
     while i < len(species_list):
-        b = gen_bulk(species_list[i], bv=bv[i], ft=ft, supcell=supcell, a=a[i])
+        b = gen_bulk(species_list[i], bv=bv[i], ft=ft, supcell=supcell, a=a[i], fix=fix)
         for facet in b.keys():
             try:
                 os.makedirs(species_list[i] + "/" + facet)
@@ -46,7 +52,9 @@ def gen_bulk_dirs(
     print("Completed")
 
 
-def gen_bulk(species, bv="fcc", ft=["100", "110", "111"], supcell=(3, 3, 4), a=None):
+def gen_bulk(
+    species, bv="fcc", ft=["100", "110", "111"], supcell=(3, 3, 4), a=None, fix=0
+):
     """
     Given species, bravais lattice, and facets, generates dict of ase objects for surfaces
 
@@ -56,6 +64,7 @@ def gen_bulk(species, bv="fcc", ft=["100", "110", "111"], supcell=(3, 3, 4), a=N
     ft (list of str): facets to be considered
     supcell (tuple): supercell size
     a (float): lattice parameter. if None uses experimental value
+    fix (int): number of layers from bottom to fix (e.g. value of 2 fixes bottom 2 layers)
 
     Returns
     bulk (dict): dictionary of generated bulk facets
@@ -78,4 +87,9 @@ def gen_bulk(species, bv="fcc", ft=["100", "110", "111"], supcell=(3, 3, 4), a=N
                 species, size=supcell, vacuum=10.0, a=a
             )
         j += 1
+    if fix > 0:
+        for sys in bulk:
+            f = FixAtoms(mask=[atom.tag > (supcell[-1] - fix) for atom in bulk[sys]])
+            bulk[sys].set_constraint([f])
+
     return bulk
