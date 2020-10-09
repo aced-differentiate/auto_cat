b8c3639c8d5bafa37b1806259f57ee2f3e9b8810
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-10-09 16:53:42 -0400

    Reads in default lattice parameters for each species from ASE by default and allows manual specification

--- src/autocat/mpea.py
+++ src/autocat/mpea.py
@@ -5,12 +5,19 @@ from ase import Atom, Atoms
 from ase.visualize import view
 from ase.build import fcc100, fcc110, fcc111
 from ase.build import bcc100, bcc110, bcc111
+from ase.data import reference_states, atomic_numbers
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.analysis.adsorption import AdsorbateSiteFinder
 
 
-def gen_mpea(
-    spec_list, comp, bv="fcc", ft=["100", "110", "111"], supcell=(3, 3, 4), samps=15
+def gen_mpea_dirs(
+    species_list,
+    comp={},
+    latts={},
+    bv="fcc",
+    ft=["100", "110", "111"],
+    supcell=(3, 3, 4),
+    samps=15,
 ):
     """
 
@@ -18,8 +25,9 @@ def gen_mpea(
     in separate directories
 
     Parameters:
-        spec_list(list of str): names of chemical species to be included
-        comp(list of floats): composition of species list
+        species_list(list of str): names of chemical species to be included
+        comp(dict): desired composition, if species in list not mentioned, assumed to be 1/sum(comp)
+        latts(dict): lattice parameters for each species (defaults to ASE values if not specified)
         bv(str): bravais lattice
         ft(list of str): facets to be considered
         supcell(tuple of int): supercell size
@@ -31,29 +39,38 @@ def gen_mpea(
     # Get MPEA name from species list and composition
     name = ""
     j = 0
-    while j < len(spec_list):
-        name += spec_list[j]
-        name += str(comp[j])
+    while j < len(species_list):
+        name += species_list[j]
+        if species_list[j] in comp:
+            name += str(comp[species_list[j]])
+        else:
+            name += str(1)
         j += 1
 
     curr_dir = os.getcwd()
 
     for f in ft:
         try:
-            os.mkdir(name + "_" + bv + f)
+            os.makedirs(name + "/" + bv + f)
         except OSError:
-            print("Failed Creating Directory ./{}".format(name + "_" + bv + f))
+            print("Failed Creating Directory ./{}".format(name + "/" + bv + f))
         else:
-            print("Successfully Created Directory ./{}".format(name + "_" + bv + f))
-            os.chdir(name + "_" + bv + f)
+            print("Successfully Created Directory ./{}".format(name + "/" + bv + f))
+            os.chdir(name + "/" + bv + f)
             sub_dir = os.getcwd()
             print("Beginning Generation of MPEAS for facet {}{}".format(bv, f))
             i = 0
             while i < samps:
                 os.mkdir(str(i + 1))
                 os.chdir(str(i + 1))
-                gen_mpea_struct(
-                    spec_list, comp, bv=bv, ft=f, supcell=supcell, write_traj=True
+                gen_mpea(
+                    species_list=species_list,
+                    comp=comp,
+                    latts=latts,
+                    bv=bv,
+                    ft=f,
+                    supcell=supcell,
+                    write_traj=True,
                 )
                 os.chdir(sub_dir)
                 i += 1
@@ -64,15 +81,22 @@ def gen_mpea(
     return None
 
 
-def gen_mpea_struct(
-    spec_list, comp, bv="fcc", ft="100", supcell=(3, 3, 4), write_traj=False
+def gen_mpea(
+    species_list,
+    comp={},
+    latts={},
+    bv="fcc",
+    ft="100",
+    supcell=(3, 3, 4),
+    write_traj=False,
 ):
     """
     Returns a random mpea traj structure given a species list and composition
 
     Parameters:
-        spec_list(list of str): list of species to include
-        comp(list of floats): desired composition
+        species_list(list of str): list of species to include
+        comp(dict): desired composition, if species in list not mentioned, assumed to be 1/sum(comp)
+        latts(dict): lattice parameters for each species (defaults to ASE values if not specified)
         bv(str): bravais lattice of structure (currently only supports fcc & bcc)
         ft(str): facet to be considered
         supcell(tuple of ints): supercell size
@@ -82,17 +106,30 @@ def gen_mpea_struct(
         mpea(ase obj): randomly generated mpea
 
     """
-    comp_sum = np.sum(comp)
-    p = np.array(comp) / comp_sum
 
-    # Optimimum lattice parameters calculated using BEEF-vdW
-    latt_beef = {"Pt": 2.014179 * 2, "Ir": 3.84}  # Need to fix Ir
+    # Checks if any of the species in the species list are specified in comp
+    # Otherwise sets it to 1
+
+    i = 0
+    comp_list = []
+    while i < len(species_list):
+        if species_list[i] not in comp:
+            comp_list.append(1.0)
+        else:
+            comp_list.append(comp[species_list[i]])
+        i += 1
+
+    comp_sum = np.sum(comp_list)
+    p = np.array(comp_list) / comp_sum
 
     # Average lattice parameter given composition
     a = 0
     i = 0
-    while i < len(spec_list):
-        a += latt_beef[spec_list[i]] * p[i]
+    while i < len(species_list):
+        if species_list[i] in latts:
+            a += latts[species_list[i]] * p[i]
+        else:
+            a += reference_states[atomic_numbers[species_list[i]]]["a"] * p[i]
         i += 1
 
     # Generate atoms list
@@ -101,7 +138,7 @@ def gen_mpea_struct(
     atoms_list = []
     i = 0
     while i < num_atoms:
-        atoms_list.append(np.random.choice(spec_list, p=p))
+        atoms_list.append(np.random.choice(species_list, p=p))
         i += 1
 
     # Build structure
@@ -115,16 +152,16 @@ def gen_mpea_struct(
         "bcc111": bcc111,
     }
 
-    host = funcs[bv + ft](spec_list[0], size=supcell, vacuum=10.0, a=a)
+    host = funcs[bv + ft](species_list[0], size=supcell, vacuum=10.0, a=a)
 
     host.set_chemical_symbols(atoms_list)
 
     if write_traj:
         name = ""
         j = 0
-        while j < len(spec_list):
-            name += spec_list[j]
-            name += str(comp[j])
+        while j < len(species_list):
+            name += species_list[j]
+            name += str(comp_list[j])
             j += 1
         host.write(name + ".i.traj")
     return host
