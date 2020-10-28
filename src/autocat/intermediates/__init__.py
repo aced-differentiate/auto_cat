60bfbf3e034d8bee789553002faf94e1cc9f7ca5
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-10-28 14:28:01 -0400

    Update to allow importing of default reaction intermediates from autocat.intermediates instead of autocat.intermediates.rxn

---
+++ src/autocat/intermediates/__init__.py
@@ -0,0 +1,4 @@
+from autocat.intermediates.nrr import nrr_mols, nrr_intermediate_names
+from autocat.intermediates.orr import orr_mols, orr_intermediate_names
+
+__all__ = ["nrr_mols", "nrr_intermediate_names", "orr_mols", "orr_intermediate_names"]
