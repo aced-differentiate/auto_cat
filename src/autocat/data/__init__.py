b88e3beed907ea56ffc3124feba9f2ace5b9b188
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-11-09 16:50:31 -0500

    Converts latt_param.py to a json

--- src/autocat/data/__init__.py
+++ src/autocat/data/__init__.py
@@ -1,3 +1,17 @@
-from autocat.data.latt_param import pbe_latts
+import json
+import pkg_resources
 
-__all__ = ["pbe_latts"]
+__all__ = ["pbe_fd"]
+
+"""
+Calculator Settings:
+
+    pbe_fd:
+        h = 0.16, kpts = (12,12,12)
+
+"""
+
+raw_pbe_fd = pkg_resources.resource_filename("autocat.data", "pbe_fd.json")
+
+with open(raw_pbe_fd) as fr:
+    pbe_fd = json.load(fr)
