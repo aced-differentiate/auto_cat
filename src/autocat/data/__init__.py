683ba3e09116cecb33c4c285e851a879819d941a
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-11-11 18:20:49 -0500

    Start collecting BEEF-vdW parameters for both fd and pw

--- src/autocat/data/__init__.py
+++ src/autocat/data/__init__.py
@@ -1,12 +1,12 @@
 import json
 import pkg_resources
 
-__all__ = ["pbe_fd", "pbe_pw"]
+__all__ = ["pbe_fd", "beefvdw_fd", "pbe_pw", "beefvdw_pw"]
 
 """
 Calculator Settings:
 
-    pbe_fd:
+    pbe_fd, beefvdw_fd:
 
         Obtained via fits to an equation of state
         (https://wiki.fysik.dtu.dk/ase/ase/eos.html)
@@ -20,7 +20,7 @@ Calculator Settings:
         fit to a Birch-Murnaghan EOS
 
 
-    pbe_pw:
+    pbe_pw, beefvdw_pw:
 
         Obtained using the Exponential Cell Filter to minimize the stress tensor and atomic forces
         (https://wiki.fysik.dtu.dk/ase/ase/constraints.html#the-expcellfilter-class)
@@ -30,6 +30,7 @@ Calculator Settings:
 
         HCP
         mode=PW(550), kpts = (12,12,6), fmax = 0.05 eV/A
+ 
 """
 
 raw_pbe_fd = pkg_resources.resource_filename("autocat.data", "pbe_fd.json")
@@ -38,7 +39,19 @@ with open(raw_pbe_fd) as fr:
     pbe_fd = json.load(fr)
 
 
+raw_beefvdw_fd = pkg_resources.resource_filename("autocat.data", "beefvdw_fd.json")
+
+with open(raw_beefvdw_fd) as fr:
+    beefvdw_fd = json.load(fr)
+
+
 raw_pbe_pw = pkg_resources.resource_filename("autocat.data", "pbe_pw.json")
 
 with open(raw_pbe_pw) as fr:
     pbe_pw = json.load(fr)
+
+
+raw_beefvdw_pw = pkg_resources.resource_filename("autocat.data", "beefvdw_pw.json")
+
+with open(raw_beefvdw_pw) as fr:
+    beefvdw_pw = json.load(fr)
