2f7c48ec0c5fa4ec719018a685a1a07ff696b086
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-11-12 17:57:30 -0500

    Restructure gen_surf_dirs into generate_surface_structures

--- src/autocat/surface.py
+++ src/autocat/surface.py
@@ -1,4 +1,10 @@
 import os
+from typing import List
+from typing import Tuple
+from typing import Dict
+from typing import Optional
+from typing import Union
+
 from ase.io import read, write
 from ase.build import fcc100, fcc110, fcc111
 from ase.build import bcc100, bcc110, bcc111
@@ -7,21 +13,26 @@ from ase.data import reference_states, atomic_numbers, ground_state_magnetic_mom
 from ase.constraints import FixAtoms
 
 
-def gen_surf_dirs(
-    species_list,
-    bv_dict={},
-    ft=["100", "110", "111"],
-    supcell=(3, 3, 4),
-    a_dict={},
-    c_dict={},
-    fix=0,
+def generate_surface_structures(
+    species_list: List[str],
+    crystal_structures: Dict[str, str] = None,
+    ft: List[str] = None,
+    supcell: Union[Tuple[int], List[int]] = (3, 3, 4),
+    a_dict: Optional[Dict[str, float]] = None,
+    c_dict: Optional[Dict[str, float]] = None,
+    set_magnetic_moments: List[str] = None,
+    magnetic_moments: Optional[Dict[str, float]] = None,
+    fix: int = 0,
+    write_to_disk: bool = False,
+    write_location: str = ".",
+    dirs_exist_ok: bool = False,
 ):
     """
     Given list of species, bravais lattice, and facets creates directories containing traj files for the surfaces
 
     Parameters:
         species_list(list of str): list of surf species to be generated
-        bv(dict): dict of manually specified bravais lattices for specific species
+        cs(dict): dict of manually specified bravais lattices for specific species
         ft(list of str): list of facets to consider (should be same length as species list)
         supcell(tuple of int): supercell size to be generated
         a(dict): manually specified lattice parameters for species. if None then uses ASE default
@@ -30,54 +41,82 @@ def gen_surf_dirs(
     Returns:
         None
     """
+
+    if crystal_structures is None:
+        crystal_structures = {}
+    if ft is None:
+        ft = ["100", "110", "111"]
+    if a_dict is None:
+        a_dict = {}
+    if c_dict is None:
+        c_dict = {}
+    if set_magnetic_moments is None:
+        set_magnetic_moments = ["Fe", "Co", "Ni"]
+    if magnetic_moments is None:
+        magnetic_moments = {}
+
+    # load crystal structure defaults from `ase.data`, override with user input
+    cs_library = {
+        species: reference_states[atomic_numbers[species]].get("symmetry")
+        for species in species_list
+    }
+    cs_library.update(crystal_structures)
+
+    # load magnetic moment defaults from `ase.data`, override with user input
+    mm_library = {
+        species: ground_state_magnetic_moments[atomic_numbers[species]]
+        for species in species_list
+    }
+    mm_library.update(magnetic_moments)
+
+    funcs = {
+        "fcc100": fcc100,
+        "fcc110": fcc110,
+        "fcc111": fcc111,
+        "bcc100": bcc100,
+        "bcc110": bcc110,
+        "bcc111": bcc111,
+        "hcp0001": hcp0001,
+    }
+
     curr_dir = os.getcwd()
-    i = 0
-    while i < len(species_list):
 
-        # Check if Bravais lattice manually specified
-        if species_list[i] in bv_dict:
-            bv = bv_dict[species_list[i]]
-        else:
-            bv = None
+    surface_structures = {}
+    for species in species_list:
+        cs = cs_library.get(species)
+        a = a_dict.get(species)
+        c = c_dict.get(species)
 
-        # Check if a manually specified
-        if species_list[i] in a_dict:
-            a = a_dict[species_list[i]]
-        else:
-            a = None
-
-        # Check if c manually specified
-        if species_list[i] in c_dict:
-            c = c_dict[species_list[i]]
-        else:
-            c = None
-
-        b = gen_surf(species_list[i], bv=bv, ft=ft, supcell=supcell, a=a, c=c, fix=fix)
-        for facet in b.keys():
-            try:
-                os.makedirs(species_list[i] + "/" + facet)
-            except OSError:
-                print(
-                    "Failed Creating Directory ./{}".format(
-                        species_list[i] + "/" + facet
-                    )
-                )
+        surf = {}
+        for facet in ft:
+            if c is not None:
+                struct = funcs[cs + facet](species, size=supcell, vacuum=10.0, a=a, c=c)
             else:
-                print(
-                    "Successfully Created Directory ./{}".format(
-                        species_list[i] + "/" + facet
-                    )
-                )
-                os.chdir(species_list[i] + "/" + facet)
-                b[facet].write(species_list[i] + "_" + facet + ".i.traj")
-                os.chdir(curr_dir)
-        i += 1
-    print("Completed")
+                struct = funcs[cs + facet](species, size=supcell, vacuum=10.0, a=a)
+
+            if fix > 0:
+                f = FixAtoms(mask=[atom.tag > (supcell[-1] - fix) for atom in struct])
+                struct.set_constraint([f])
+
+            if species in set_magnetic_moments:
+                struct.set_initial_magnetic_moments([mm_library[species]] * len(struct))
+
+            traj_file_path = None
+            if write_to_disk:
+                dir_path = os.path.join(write_location, f"{species}/{cs}{facet}")
+                os.makedirs(dir_path, exist_ok=dirs_exist_ok)
+                traj_file_path = os.path.join(dir_path, "input.traj")
+                struct.write(traj_file_path)
+
+            surf[cs + facet] = {"structure": struct, "traj_file_path": traj_file_path}
+
+        surface_structures[species] = surf
+    return surface_structures
 
 
 def gen_surf(
     species,
-    bv=None,
+    cs=None,
     ft=["100", "110", "111"],
     supcell=(3, 3, 4),
     a=None,
@@ -90,7 +129,7 @@ def gen_surf(
 
     Parameters
     species (str): surf species
-    bv (str): bravais lattice
+    cs (str): bravais lattice
     ft (list of str): facets to be considered
     supcell (tuple): supercell size
     a (float): lattice parameter. if None uses ASE default
@@ -101,8 +140,8 @@ def gen_surf(
     surf (dict): dictionary of generated surf facets
     """
 
-    if bv is None:  # uses ASE data to get Bravais Lattice
-        bv = reference_states[atomic_numbers[species]]["symmetry"]
+    if cs is None:  # uses ASE data to get Bravais Lattice
+        cs = reference_states[atomic_numbers[species]]["symmetry"]
 
     surf = {}
     funcs = {
@@ -117,11 +156,11 @@ def gen_surf(
     j = 0
     while j < len(ft):
         if c is not None:
-            surf[bv + ft[j]] = funcs[bv + ft[j]](
+            surf[cs + ft[j]] = funcs[cs + ft[j]](
                 species, size=supcell, vacuum=10.0, a=a, c=c
             )
         else:
-            surf[bv + ft[j]] = funcs[bv + ft[j]](
+            surf[cs + ft[j]] = funcs[cs + ft[j]](
                 species, size=supcell, vacuum=10.0, a=a
             )
         j += 1
