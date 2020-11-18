0be4f721702410ee85fd69488e95d2328e733e20
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-11-18 15:21:13 -0500

    Adds saa tests for host species, dopant species, and writing to disk

--- tests/test_saa.py
+++ tests/test_saa.py
@@ -1,13 +1,55 @@
-import unittest
+"""Unit tests for the `autocat.saa` module."""
+
 import os
 import shutil
-from autocat import gen_saa
+
+import pytest
+from pytest import approx
+from pytest import raises
+
+import numpy as np
+from autocat.saa import generate_saa_structures
 
 
-class TestSAA(unittest.TestCase):
-    def test_CuFe(self):
-        gen_saa(["Cu"], ["Fe"], supcell=(5, 5, 4), a=1.813471 * 2)
+def test_generate_saa_structures_host_species_list():
+    saa = generate_saa_structures(["Pt", "Cu"], ["Fe"])
+    assert "Pt" in saa
+    assert "Cu" in saa
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_generate_saa_structures_dops_species_list():
+    # Test dopants added to each host
+    saa = generate_saa_structures(["Pt", "Cu"], ["Fe"])
+    assert "Fe" in saa["Pt"]
+    assert "Fe" in saa["Cu"]
+    # Test dopant species not added to itself
+    saa = generate_saa_structures(["Pt", "Cu"], ["Fe", "Cu"])
+    assert "Cu" not in saa["Cu"]
+
+
+def test_generate_saa_structures_write_location():
+    # Test user-specified write location
+    saa = generate_saa_structures(
+        ["Pt", "Cu"], ["Fe"], write_to_disk=True, write_location="test_dir"
+    )
+    assert os.path.samefile(
+        saa["Pt"]["Fe"]["fcc111"]["traj_file_path"], "test_dir/Pt/Fe/fcc111/input.traj"
+    )
+    assert os.path.samefile(
+        saa["Cu"]["Fe"]["fcc100"]["traj_file_path"], "test_dir/Cu/Fe/fcc100/input.traj"
+    )
+    shutil.rmtree("test_dir")
+
+
+def test_generate_saa_structures_dirs_exist_ok():
+    saa = generate_saa_structures(["Pt", "Cu"], ["Fe"], write_to_disk=True)
+    with raises(FileExistsError):
+        saa = generate_saa_structures(["Pt", "Cu"], ["Fe"], write_to_disk=True)
+    saa = generate_saa_structures(
+        ["Pt", "Cu"], ["Fe"], write_to_disk=True, dirs_exist_ok=True
+    )
+    assert os.path.samefile(
+        saa["Pt"]["Fe"]["fcc110"]["traj_file_path"], "Pt/Fe/fcc110/input.traj"
+    )
+    shutil.rmtree("Pt")
+    shutil.rmtree("Cu")
