134767fe0f0e30148d64de3b4228f0fff090a059
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2020-11-23 13:04:56 -0500

    Adds mpea tests for facets and writing to disk

--- tests/test_mpea.py
+++ tests/test_mpea.py
@@ -26,3 +26,45 @@ def test_generate_mpea_random_samples():
     # Test that the number of samples specified are generated
     mpeas = generate_mpea_random(["Pt", "Pd", "Ir", "Cu"], num_of_samples=20)
     assert len(list(mpeas["fcc110"].keys())) == 20
+
+
+def test_generate_mpea_random_facets():
+    # Test default facets given a crystal structure
+    mpeas = generate_mpea_random(["Pt", "Pd", "Ir", "Cu"], crystal_structure="fcc")
+    with raises(KeyError):
+        m = mpeas["bcc100"]
+    assert list(mpeas.keys()) == ["fcc100", "fcc111", "fcc110"]
+    mpeas = generate_mpea_random(["Pt", "Pd", "Ir", "Cu"], crystal_structure="bcc")
+    assert list(mpeas.keys()) == ["bcc100", "bcc111", "bcc110"]
+    # Test manually specified facets
+    mpeas = generate_mpea_random(
+        ["Pt", "Pd", "Ir", "Cu"], crystal_structure="bcc", facets=["111"]
+    )
+    with raises(KeyError):
+        m = mpeas["bcc110"]
+
+
+def test_generate_mpea_random_write_location():
+    # Test user-specified write location
+    mpeas = generate_mpea_random(
+        ["Pt", "Pd", "Ir", "Cu"], write_to_disk=True, write_location="test_dir"
+    )
+    assert os.path.samefile(
+        mpeas["fcc111"]["1"]["traj_file_path"],
+        "test_dir/Pt1.0Pd1.0Ir1.0Cu1.0/fcc111/1/input.traj",
+    )
+    shutil.rmtree("test_dir")
+
+
+def test_generate_mpea_random_dirs_exist_ok():
+    mpeas = generate_mpea_random(["Pt", "Pd", "Ir", "Cu"], write_to_disk=True)
+    with raises(FileExistsError):
+        mpeas = generate_mpea_random(["Pt", "Pd", "Ir", "Cu"], write_to_disk=True)
+    mpeas = generate_mpea_random(
+        ["Pt", "Pd", "Ir", "Cu"], write_to_disk=True, dirs_exist_ok=True
+    )
+    assert os.path.samefile(
+        mpeas["fcc100"]["5"]["traj_file_path"],
+        "Pt1.0Pd1.0Ir1.0Cu1.0/fcc100/5/input.traj",
+    )
+    shutil.rmtree("Pt1.0Pd1.0Ir1.0Cu1.0")
