ba8ff3e2bc266dbb6b40eeccd031ac28391d4056
Vinay Hegde <vhegde@citrine.io>
2020-12-18 20:11:52 -0800

    refactor acc chg in module variables; more expressive var names

--- src/autocat/surface.py
+++ src/autocat/surface.py
@@ -1,31 +1,31 @@
 import os
 from typing import List
-from typing import Tuple
 from typing import Dict
 from typing import Optional
-from typing import Union
 
-from ase.io import read, write
-from ase.build import fcc100, fcc110, fcc111
-from ase.build import bcc100, bcc110, bcc111
-from ase.build import hcp0001
-from ase.data import reference_states, atomic_numbers, ground_state_magnetic_moments
+import ase.build
+from ase.data import reference_states
+from ase.data import atomic_numbers
+from ase.data import ground_state_magnetic_moments
 from ase.constraints import FixAtoms
-from autocat.data import *
+from autocat.data.lattice_parameters import BULK_PBE_FD
+from autocat.data.lattice_parameters import BULK_PBE_PW
+from autocat.data.lattice_parameters import BULK_BEEFVDW_FD
+from autocat.data.lattice_parameters import BULK_BEEFVDW_PW
 
 
 def generate_surface_structures(
     species_list: List[str],
     crystal_structures: Dict[str, str] = None,
-    ft_dict: Dict[str, str] = None,
-    supcell: Union[Tuple[int], List[int]] = (3, 3, 4),
-    default_lattice_library: str = "ase",
+    facets: Dict[str, str] = None,
+    supercell_dim: List[int] = None,
+    default_lat_param_lib: str = None,
     a_dict: Optional[Dict[str, float]] = None,
     c_dict: Optional[Dict[str, float]] = None,
     set_magnetic_moments: List[str] = None,
     magnetic_moments: Optional[Dict[str, float]] = None,
-    vac: float = 10.0,
-    fix: int = 0,
+    vacuum: float = 10.0,
+    n_fixed_layers: int = 0,
     write_to_disk: bool = False,
     write_location: str = ".",
     dirs_exist_ok: bool = False,
@@ -37,49 +37,46 @@ def generate_surface_structures(
     Parameters
     ----------
 
-    species_list:
-        List of chemical symbols of the slabs to be built
+    species_list (REQUIRED):
+        List of chemical symbols of the slabs to be built.
 
     crystal_structures:
         Dictionary with crystal structure to be used for each species.
         Options are fcc, bcc, or hcp. If not specified, will use the
-        default reference crystal for each species from `ase.data`.
-    
-    ft_dict:
+
+    facets:
         Dictionary with the surface facets to be considered for each
-        species. 
+        species.
 
         If not specified for a given species, the following
         defaults will be used based on the crystal structure:
-        fcc/bcc: 100,111,110
+        fcc/bcc: 100, 111, 110
         hcp: 0001
-        
-    supcell: 
-        Tuple or List specifying the size of the supercell to be
-        generated in the format (nx,ny,nz).
 
-    default_lattice_library:
+    supercell_dim:
+        List specifying the size of the supercell to be generated in the
+        format (nx, ny, nz).
+
+    default_lat_param_lib:
         String indicating which library the lattice constants should be pulled
         from if not specified in either a_dict or c_dict. Defaults to ase.
 
         Options are:
-        ase: defaults given in `ase.data`
-        pbe_fd: parameters calculated using xc=pbe and finite-difference
+        pbe_fd: parameters calculated using xc=PBE and finite-difference
         beefvdw_fd: parameters calculated using xc=BEEF-vdW and finite-difference
-        pbe_pw: parameters calculated using xc=pbe and a plane-wave basis set
+        pbe_pw: parameters calculated using xc=PBE and a plane-wave basis set
         beefvdw_fd: parameters calculated using xc=BEEF-vdW and a plane-wave basis set
 
         N.B. if there is a species present in species_list that is NOT in the
         reference library specified, it will be pulled from `ase.data`
 
-
     a_dict:
         Dictionary with lattice parameters <a> to be used for each species.
-        If not specified, defaults from default_lattice_library are used.
+        If not specified, defaults from default_lat_param_lib are used.
 
     c_dict:
         Dictionary with lattice parameters <c> to be used for each species.
-        If not specified, defaults from default_lattice_library are used.
+        If not specified, defaults from default_lat_param_lib are used.
 
     set_magnetic_moments:
         List of species for which magnetic moments need to be set.
@@ -90,16 +87,16 @@ def generate_surface_structures(
         Dictionary with the magnetic moments to be set for the chemical
         species listed previously.
         If not specified, default ground state magnetic moments from
-        `ase.data` are used. 
+        `ase.data` are used.
 
-    vac:
-        Float specifying the amount of vacuum to be added on each
-        side of the slab.
+    vacuum:
+        Float specifying the amount of vacuum (in Angstrom) to be added on
+        each side of the slab.
 
-    fix:
+    n_fixed_layers:
         Integer giving the number of layers of the slab to be fixed
-        starting from the bottom up. (e.g. a value of 2 will fix the
-        bottom 2 layers)
+        starting from the bottom up (e.g. a value of 2 will fix the
+        bottom 2 layers).
 
     write_to_disk:
         Boolean specifying whether the bulk structures generated should be
@@ -113,6 +110,7 @@ def generate_surface_structures(
         In the specified write_location, the following directory structure
         will be created:
         [species]/[crystal_structure + facet]/substrate/input.traj
+        Defaults to the current working directory.
 
     dirs_exist_ok:
         Boolean specifying whether existing directories/files should be
@@ -122,45 +120,42 @@ def generate_surface_structures(
 
     Returns
     -------
- 
+
     Dictionary with surface structures (as `ase.Atoms` objects) and
     write-location (if any) for each input species.
 
     """
 
-    latt_const_libraries = {
-        "pbe_fd": pbe_fd,
-        "beefvdw_fd": beefvdw_fd,
-        "pbe_pw": pbe_pw,
-        "beefvdw_pw": beefvdw_pw,
+    lpl = {
+        "pbe_fd": BULK_PBE_FD,
+        "beefvdw_fd": BULK_BEEFVDW_FD,
+        "pbe_pw": BULK_PBE_PW,
+        "beefvdw_pw": BULK_BEEFVDW_PW,
+    }
+    ase_build_funcs = {
+        "fcc100": ase.build.fcc100,
+        "fcc110": ase.build.fcc110,
+        "fcc111": ase.build.fcc111,
+        "bcc100": ase.build.bcc100,
+        "bcc110": ase.build.bcc110,
+        "bcc111": ase.build.bcc111,
+        "hcp0001": ase.build.hcp0001,
     }
 
     if crystal_structures is None:
         crystal_structures = {}
+    if facets is None:
+        facets = {}
+    if supercell_dim is None:
+        supercell_dim = [3, 3, 4]
     if a_dict is None:
         a_dict = {}
-    if default_lattice_library != "ase":
-        lib = latt_const_libraries[default_lattice_library]
-        a_dict.update(
-            {species: lib[species]["a"] for species in lib if species not in a_dict}
-        )
     if c_dict is None:
         c_dict = {}
-    if default_lattice_library != "ase":
-        lib = latt_const_libraries[default_lattice_library]
-        c_dict.update(
-            {
-                species: lib[species]["c"]
-                for species in lib
-                if species not in c_dict and "c" in lib[species]
-            }
-        )
     if set_magnetic_moments is None:
         set_magnetic_moments = ["Fe", "Co", "Ni"]
     if magnetic_moments is None:
         magnetic_moments = {}
-    if ft_dict is None:
-        ft_dict = {}
 
     # load crystal structure defaults from `ase.data`, override with user input
     cs_library = {
@@ -169,22 +164,31 @@ def generate_surface_structures(
     }
     cs_library.update(crystal_structures)
 
+    # load lattice params <a>, <c> from reference library, override with user input
+    a_library = {}
+    c_library = {}
+    if default_lat_param_lib is not None:
+        a_library.update(
+            {
+                species: lpl[default_lat_param_lib].get(species, {}).get("a")
+                for species in species_list
+            }
+        )
+        c_library.update(
+            {
+                species: lpl[default_lat_param_lib].get(species, {}).get("c")
+                for species in species_list
+            }
+        )
+    a_library.update(a_dict)
+    c_library.update(c_dict)
+
     # load magnetic moment defaults from `ase.data`, override with user input
     mm_library = {
         species: ground_state_magnetic_moments[atomic_numbers[species]]
         for species in species_list
     }
     mm_library.update(magnetic_moments)
-
-    funcs = {
-        "fcc100": fcc100,
-        "fcc110": fcc110,
-        "fcc111": fcc111,
-        "bcc100": bcc100,
-        "bcc110": bcc110,
-        "bcc111": bcc111,
-        "hcp0001": hcp0001,
-    }
 
     # set default facets for each crystal structure, override with user input
     ft_defaults = {
@@ -194,23 +198,32 @@ def generate_surface_structures(
     }
 
     ft_library = {species: ft_defaults[cs_library[species]] for species in species_list}
-    ft_library.update(ft_dict)
+    ft_library.update(facets)
 
     surface_structures = {}
     for species in species_list:
         cs = cs_library.get(species)
-        a = a_dict.get(species)
-        c = c_dict.get(species)
+        a = a_library.get(species)
+        c = c_library.get(species)
 
         surf = {}
         for facet in ft_library[species]:
             if c is not None:
-                struct = funcs[cs + facet](species, size=supcell, vacuum=vac, a=a, c=c)
+                struct = ase_build_funcs[f"{cs}{facet}"](
+                    species, size=supercell_dim, vacuum=vacuum, a=a, c=c
+                )
             else:
-                struct = funcs[cs + facet](species, size=supcell, vacuum=vac, a=a)
+                struct = ase_build_funcs[f"{cs}{facet}"](
+                    species, size=supercell_dim, vacuum=vacuum, a=a
+                )
 
-            if fix > 0:
-                f = FixAtoms(mask=[atom.tag > (supcell[-1] - fix) for atom in struct])
+            if n_fixed_layers > 0:
+                f = FixAtoms(
+                    mask=[
+                        atom.tag > (supercell_dim[-1] - n_fixed_layers)
+                        for atom in struct
+                    ]
+                )
                 struct.set_constraint([f])
 
             if species in set_magnetic_moments:
@@ -219,7 +232,7 @@ def generate_surface_structures(
             traj_file_path = None
             if write_to_disk:
                 dir_path = os.path.join(
-                    write_location, f"{species}/{cs}{facet}/substrate"
+                    write_location, f"{species}", f"{cs}{facet}", "substrate"
                 )
                 os.makedirs(dir_path, exist_ok=dirs_exist_ok)
                 traj_file_path = os.path.join(dir_path, "input.traj")
