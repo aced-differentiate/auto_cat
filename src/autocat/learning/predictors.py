6b749c9e48e0954038e5fa60b9c501938d7e6f1c
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-02-16 17:08:51 -0500

    add wrapper for qml cholesky decomp

--- src/autocat/learning/predictors.py
+++ src/autocat/learning/predictors.py
@@ -1,5 +1,45 @@
 import qml.kernels as qml_kernels
+import qml.math as qml_math
 import numpy as np
+
+
+def get_alphas(
+    kernel: np.ndarray,
+    perturbation_matrices: np.ndarray,
+    regularization_strength: float = 1e-8,
+):
+    """
+    Wrapper for `qml.math.cho_solve` to
+    solve for the regression coefficients using cholesky
+    decomposition (ie. training a KRR)
+
+    alphas = (K + lambda*I)^(-1)y
+
+    Parameters
+    ----------
+
+    kernel:
+        Numpy array of kernel matrix (e.g. generated via `get_kernel`)
+
+    perturbation_matrices:
+        Numpy array containing perturbation matrices corresponding to the
+        input structures which form the labels when training
+
+    regularization_strength:
+        Float specifying regularization strength (lambda in eqtn above)
+
+    perturbation
+
+    Returns
+    -------
+
+    alphas:
+        Numpy array of regression coefficients
+
+    """
+    K = kernel + regularization_strength * np.identity(kernel.shape)
+
+    return qml_math.cho_solve(K, perturbation_matrices)
 
 
 def get_kernel(X1: np.ndarray, X2: np.ndarray, kernel_type: str = "gaussian", **kwargs):
