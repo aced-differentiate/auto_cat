54d64f98231991b1d047092d725ed7ea3d618adf
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-02-24 15:46:49 -0500

    adds rmg.molecule -> ase.Atoms converter

---
+++ src/autocat/io/rmg.py
@@ -0,0 +1,32 @@
+from ase.io import read
+from rmgpy.molecule.converter import to_rdkit_mol
+from rmgpy.chemkin import load_species_dictionary
+from rdkit.Chem.rdmolfiles import SDWriter
+
+import tempfile
+import os
+
+
+def rmgmol_to_ase_atoms(rmgmol):
+    """
+    Converts an rmgpy Molecule object to an ase Atoms object
+
+    Parameters
+    ----------
+
+    rmgmol:
+        rmg Molecule object to be converted
+
+    Returns
+    -------
+
+    aseobj:
+        Atoms object
+    """
+    rd = to_rdkit_mol(rmgmol, remove_h=False, sanitize=False)
+    with tempfile.TemporaryDirectory() as _tmp_dir:
+        writer = SDWriter(os.path.join(_tmp_dir, "tmp.sdf"))
+        writer.write(rd)
+        writer.close()
+        ase_mol = ase_read("test.sdf")
+        return ase_mol
