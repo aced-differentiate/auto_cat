50f73f117c59064fdf0a11e4f57118966777c06e
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-04-17 21:44:30 -0400

    allows picking N candidates with highest unc

--- src/autocat/learning/sequential.py
+++ src/autocat/learning/sequential.py
@@ -14,6 +14,10 @@ from autocat.learning.predictors import AutoCatStructureCorrector
 Array = List[float]
 
 
+class AutoCatSequentialLearningError(Exception):
+    pass
+
+
 def simulated_sequential_learning(
     structure_corrector: AutoCatStructureCorrector,
     training_base_structures: List[Atoms],
@@ -22,6 +26,7 @@ def simulated_sequential_learning(
     maximum_perturbation_distance: float = 0.75,
     initial_num_of_perturbations_per_base_structure: int = None,
     batch_num_of_perturbations_per_base_structure: int = 2,
+    batch_size_to_add: int = 1,
     number_of_sl_loops: int = 100,
     write_to_disk: bool = False,
     write_location: str = ".",
@@ -61,6 +66,12 @@ def simulated_sequential_learning(
         Integer giving the number of perturbations to generate
         on each loop when finding the next candidate
 
+    batch_size_to_add:
+        Integer giving the number of candidates to be added
+        to the training set on each loop. (ie. N candidates with
+        the highest uncertainties added for each iteration)
+        Default: 1 (ie. adds candidate with max unc on each loop)
+
     number_of_sl_loops:
         Integer specifying the number of sequential learning loops to be conducted
 
@@ -79,11 +90,24 @@ def simulated_sequential_learning(
         the calculation:
         - maximum uncertainty history
         - full uncertainty history
-        - predicted corrections history
-        - real corrections history
-        - mae history
-        - rmse history
+        - predicted corrections training history
+        - real corrections training history
+        - mae training history
+        - rmse training history
+        - mae testing history (if test structures given)
+        - rmse testing history (if test structures given)
+        For the corrections histories, the dimensions are as follows:
+        num of loops -> num of candidates added -> corrections applied
     """
+    if (
+        batch_num_of_perturbations_per_base_structure * len(training_base_structures)
+        < batch_size_to_add
+    ):
+        msg = (
+            "Batch size to add must be greater than the number of candidates generated"
+        )
+        raise AutoCatSequentialLearningError(msg)
+
     if initial_num_of_perturbations_per_base_structure is None:
         initial_num_of_perturbations_per_base_structure = (
             batch_num_of_perturbations_per_base_structure
@@ -164,16 +188,20 @@ def simulated_sequential_learning(
         full_unc_history.append(_uncs)
 
         # find candidate with highest uncertainty
-        high_unc_idx = np.argmax(_uncs)
+        high_unc_idx = np.argsort(_uncs)[-batch_size_to_add:]
         max_unc_history.append(_uncs[high_unc_idx])
 
-        next_candidate_struct = new_pert_structs[high_unc_idx]
+        next_candidate_struct = [new_pert_structs[idx] for idx in high_unc_idx]
         # add new perturbed struct to training set
-        train_pert_structures.append(next_candidate_struct)
-        train_pert_corr_list.append(new_pert_corr_list[high_unc_idx],)
+        train_pert_structures.extend(next_candidate_struct)
+        train_pert_corr_list.extend([new_pert_corr_list[idx] for idx in high_unc_idx],)
         # keeps as lists to make writing to disk easier
-        pred_corrs_history.append([p.tolist() for p in _pred_corrs[high_unc_idx]])
-        real_corrs_history.append(new_pert_corr_list[high_unc_idx])
+        pred_corrs_history.append(
+            [p.tolist() for p in [_pred_corrs[idx] for idx in high_unc_idx]]
+        )
+        real_corrs_history.append(
+            [new_pert_corr_list[idx].tolist() for idx in high_unc_idx]
+        )
         structure_corrector.fit(
             train_pert_structures, corrections_list=train_pert_corr_list
         )
@@ -181,7 +209,7 @@ def simulated_sequential_learning(
         "max_unc_history": [mu.tolist() for mu in max_unc_history],
         "full_unc_history": [unc.tolist() for unc in full_unc_history],
         "pred_corrs_history": pred_corrs_history,
-        "real_corrs_history": [r.tolist() for r in real_corrs_history],
+        "real_corrs_history": [r for r in real_corrs_history],
         "mae_train_history": mae_train_history,
         "rmse_train_history": rmse_train_history,
     }
--- tests/learning/test_sequential.py
+++ tests/learning/test_sequential.py
@@ -53,7 +53,45 @@ def test_simulated_sequential_outputs():
     assert len(sl_dict["pred_corrs_history"][-1]) == len(
         sl_dict["real_corrs_history"][-1]
     )
-    assert len(sl_dict["pred_corrs_history"][0]) == 2
+    assert len(sl_dict["pred_corrs_history"]) == 4
+    assert len(sl_dict["real_corrs_history"]) == 4
+
+
+def test_simulated_sequential_batch_added():
+    # Tests adding N candidates on each loop
+    sub1 = generate_surface_structures(["Pt"], facets={"Pt": ["111"]})["Pt"]["fcc111"][
+        "structure"
+    ]
+    sub2 = generate_surface_structures(["Cu"], facets={"Cu": ["100"]})["Cu"]["fcc100"][
+        "structure"
+    ]
+    base_struct1 = place_adsorbate(sub1, "OH")["custom"]["structure"]
+    base_struct2 = place_adsorbate(sub2, "NH")["custom"]["structure"]
+    acsc = AutoCatStructureCorrector(structure_featurizer="elemental_property")
+    bsta = 2
+    num_loops = 3
+    sl_dict = simulated_sequential_learning(
+        acsc,
+        [base_struct1, base_struct2],
+        batch_num_of_perturbations_per_base_structure=4,
+        batch_size_to_add=bsta,
+        number_of_sl_loops=num_loops,
+    )
+    # each max unc history should be equal to number of candidates added
+    assert len(sl_dict["max_unc_history"][0]) == bsta
+    # first dimension is number of loops
+    assert len(sl_dict["pred_corrs_history"]) == num_loops
+    # next dimension is size of candidates added on each loop
+    assert len(sl_dict["pred_corrs_history"][0]) == bsta
+    # next dimension is size of adsorbate
+    assert len(sl_dict["pred_corrs_history"][0][0]) == 2
+    # next dimension is vector correction to atom in adsorbate
+    assert len(sl_dict["pred_corrs_history"][0][0][0]) == 3
+    # check same holds for real history
+    assert len(sl_dict["real_corrs_history"]) == num_loops
+    assert len(sl_dict["real_corrs_history"][0]) == bsta
+    assert len(sl_dict["real_corrs_history"][0][0]) == 2
+    assert len(sl_dict["real_corrs_history"][0][0][0]) == 3
 
 
 def test_simulated_sequential_outputs_testing():
