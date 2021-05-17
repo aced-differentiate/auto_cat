9909170ae2e196272d62d738b8b65875e30bdace
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-05-17 18:42:33 -0400

    various updates to converter
    - allows optimization of conformers
    - allows returning all conformers or lowest energy one
    - optimization optional
    - rm X atoms from surface species

--- src/autocat/io/rmg.py
+++ src/autocat/io/rmg.py
@@ -1,8 +1,16 @@
-from ase.io import read
-from rmgpy.molecule.converter import to_rdkit_mol
+from ase.io import read as ase_read
+from ase import Atoms
 from rmgpy.molecule.molecule import Molecule
+
 from rmgpy.chemkin import load_species_dictionary
 from rdkit.Chem.rdmolfiles import SDWriter
+from arc.species.conformers import embed_rdkit
+from arc.species.conformers import rdkit_force_field
+from arc.species.converter import xyz_to_xyz_file_format
+
+from typing import List
+
+import numpy as np
 
 import tempfile
 import os
@@ -32,39 +40,45 @@ def output_yaml_to_surface_rmg_mol(output_yaml: str):
     with open(output_yaml, "r") as f:
         out_yml = yaml.load(f)
 
-    for phase in out_yml:
-        if out_yml["Phases"][phase].get("name") == "surface":
-            surfaces = out_yml["Phases"][phase]["Species"]
+    for phase in out_yml["Phases"]:
+        if phase.get("name") == "surface":
+            surfaces = phase["Species"]
 
     rmg_surf_dict = {}
     for spec in surfaces:
+        rmgmol = Molecule().from_adjacency_list(spec["adjlist"])
+        print(spec["name"])
+        if rmgmol.is_surface_site():
+            continue
         name = spec["name"]
         rmg_surf_dict[name] = {}
         rmg_surf_dict[name]["raw_dict"] = spec
-        rmgmol = Molecule().from_adjacency_list
         rmg_surf_dict[name]["rmg_mol"] = rmgmol
-        rmg_surf_dict[name]["ase_obj"] = rmgmol_to_ase_atoms(rmgmol)
+        opt = _whether_to_optimize_conformer(_remove_x(rmgmol))
+        rmg_surf_dict[name]["ase_obj"] = rmgmol_to_ase_atoms_list(
+            rmgmol, return_only_lowest_energy_conformer=True, optimize=opt,
+        )[0]
 
     return rmg_surf_dict
 
 
 def load_organized_species_dictionary(species_dictionary_location: str = "."):
     """
-    Wrapper for `rmgpy.chemkin.load_species_dictionary` which reorganizes
-    by gas and adsorbed phase species
+   Wrapper for `rmgpy.chemkin.load_species_dictionary` which reorganizes
+   by gas and adsorbed phase species
 
-    Parameters
-    ----------
+   Parameters
+   ----------
 
-    species_dictionary_location:
-        String giving path to `species_dictionary.txt` to be read
+   species_dictionary_location:
+       String giving path to `species_dictionary.txt` to be read
 
-    Returns
-    -------
+   Returns
+   -------
 
-    organized_dict:
-        Dictionary of species organized by phase 
-    """
+   organized_dict:
+       Dictionary of species organized by phase
+   """
     dict_location = os.path.join(species_dictionary_location, "species_dictionary.txt")
     raw_dict = load_species_dictionary(dict_location)
     organized_dict = {"gas": {}, "adsorbed": {}}
@@ -76,9 +90,17 @@ def load_organized_species_dictionary(species_dictionary_location: str = "."):
     return organized_dict
 
 
-def rmgmol_to_ase_atoms(rmgmol: Molecule):
+def rmgmol_to_ase_atoms_list(
+    rmgmol: Molecule,
+    num_conformers: int = 1,
+    force_field: str = "MMFF94s",
+    return_only_lowest_energy_conformer: bool = True,
+    optimize: bool = True,
+) -> List[Atoms]:
     """
-    Converts an rmgpy Molecule object to an ase Atoms object
+    Converts an `rmgpy` Molecule object to a list of `ase.Atoms` object.
+
+    Optionally optimizes the conformers using an `RDKit` MMFF forcefield.
 
     Parameters
     ----------
@@ -86,17 +108,84 @@ def rmgmol_to_ase_atoms(rmgmol: Molecule):
     rmgmol:
         rmg Molecule object to be converted
 
+    num_conformers:
+        Number of conformers to be generated
+
+    force_field:
+        String indicating RDKit MMFF force field to use
+        Options:
+        - MMFF94
+        - MMFF94s
+
+    return_only_lowest_energy_conformer:
+        Bool indicating whether to return an `ase.Atoms` object for only
+        the lowest energy conformer (only valid if `optimize` = True)
+
+    optimize:
+        Bool indicating whether to optimize the conformers or just directly convert to
+        `ase.Atoms` objects. If set to False, will always only return a single conformer
+
     Returns
     -------
 
     aseobj:
-        Atoms object
+        List of Atoms objects
     """
-    rd = to_rdkit_mol(rmgmol, remove_h=False, sanitize=False)
+    rmgmol = _remove_x(rmgmol)
+    rdkit_mol = embed_rdkit("_", rmgmol, num_confs=num_conformers)
+
     with tempfile.TemporaryDirectory() as _tmp_dir:
-        file_path = os.path.join(_tmp_dir, "tmp.sdf")
-        writer = SDWriter(file_path)
-        writer.write(rd)
-        writer.close()
-        ase_mol = read(file_path)
-        return ase_mol
+        if not optimize:
+            return _convert_unoptimized(rdkit_mol, _tmp_dir)
+
+        else:
+            xyzs, energies = rdkit_force_field("_", rdkit_mol, force_field=force_field)
+            if not energies:
+                print("Warning: Unable to optimize, returning unoptimized structure")
+                return _convert_unoptimized(rdkit_mol, _tmp_dir)
+            if return_only_lowest_energy_conformer:
+                xyzs = [xyzs[np.argmin(energies)]]
+            file_path = os.path.join(_tmp_dir, "tmp.xyz")
+            aseobj = []
+            for xyz in xyzs:
+                string = xyz_to_xyz_file_format(xyz)
+                with open(file_path, "w") as f:
+                    f.write(string)
+                aseobj.append(ase_read(file_path))
+            return aseobj
+
+
+def _remove_x(rmgmol):
+    """
+    Removes X from rmgmol object
+    """
+    if rmgmol.contains_surface_site():
+        x_idx = [s.symbol for s in rmgmol.atoms].index("X")
+        x = rmgmol.atoms[x_idx]
+        rmgmol.remove_atom(x)
+        _remove_x(rmgmol)
+    return rmgmol
+
+
+def _whether_to_optimize_conformer(rmgmol):
+    """
+    If a single isolated atom, sets optimizer to False
+    """
+    atoms = rmgmol.atoms
+    print(atoms)
+    if len(atoms) > 1:
+        return True
+    return False
+
+
+def _convert_unoptimized(rdkit_mol, _tmp_dir):
+    """
+    Converts an embedded RDKit Molecule directly to an ase.Atoms obj
+    without optimization
+    """
+    file_path = os.path.join(_tmp_dir, "tmp.sdf")
+    writer = SDWriter(file_path)
+    writer.write(rdkit_mol)
+    writer.close()
+    aseobj = [ase_read(file_path)]
+    return aseobj
--- tests/io/test_rmg.py
+++ tests/io/test_rmg.py
@@ -1,24 +1,56 @@
 """Unit tests for the `autocat.io.rmg` module."""
 
+import ase
 import numpy as np
 import os
 
 import pytest
 
 from ase import Atoms
-from rmgpy.molecule.converter import to_rdkit_mol
 from rmgpy.molecule.molecule import Molecule
-from rmgpy.chemkin import load_species_dictionary
-from rdkit.Chem.rdmolfiles import SDWriter
 
-from autocat.io.rmg import rmgmol_to_ase_atoms
+from autocat.io.rmg import rmgmol_to_ase_atoms_list
 from autocat.io.rmg import output_yaml_to_surface_rmg_mol
+from autocat.io.rmg import _remove_x
 
 
-def test_rmgmol_to_ase_atoms():
+def test_rmgmol_to_ase_atoms_list():
     # Tests converting an rmg molecule object to an ase Atoms object
     rmgmol = Molecule().from_smiles("O=C=O")
     assert isinstance(rmgmol, Molecule)
-    ase_atoms = rmgmol_to_ase_atoms(rmgmol)
-    assert isinstance(ase_atoms, Atoms)
-    assert ase_atoms.get_chemical_formula() == "CO2"
+    ase_atoms_list = rmgmol_to_ase_atoms_list(rmgmol)
+    assert isinstance(ase_atoms_list[0], Atoms)
+    assert ase_atoms_list[0].get_chemical_formula() == "CO2"
+    # Test returning multiple conformers
+    ase_atoms_list = rmgmol_to_ase_atoms_list(
+        rmgmol, num_conformers=3, return_only_lowest_energy_conformer=False
+    )
+    assert len(ase_atoms_list) == 3
+    assert isinstance(ase_atoms_list[-1], Atoms)
+    # Test without optimization
+    rmgmol = Molecule().from_smiles("O")
+    ase_atoms_list = rmgmol_to_ase_atoms_list(rmgmol, optimize=False, num_conformers=2)
+    assert len(ase_atoms_list) == 1
+    assert isinstance(ase_atoms_list[0], Atoms)
+    assert ase_atoms_list[0].get_chemical_formula() == "H2O"
+
+
+def test_remove_x():
+    # Tests removing X from an rmgmol object
+    adj_list = "1 O u0 p2 c0 {2,D}\n2 X u0 p0 c0 {1,D}\n"
+    rmgmol = Molecule().from_adjacency_list(adj_list)
+    rm_rmgmol = _remove_x(rmgmol)
+    assert not rm_rmgmol.contains_surface_site()
+    rmgmol = Molecule().from_smiles("C")
+    assert rmgmol == _remove_x(rmgmol)
+    assert not _remove_x(rmgmol).contains_surface_site()
+    # Tests with multiple Xs
+    rmgmol = Molecule().from_adjacency_list(
+        "1 O u0 p2 c0 {2,S} {5,S}\n\
+        2 C u0 p0 c0 {1,S} {3,S} {4,D}\n\
+        3 H u0 p0 c0 {2,S}\n\
+        4 X u0 p0 c0 {2,D}\n\
+        5 X u0 p0 c0 {1,S}\n"
+    )
+    rm_rmgmol = _remove_x(rmgmol)
+    assert not rm_rmgmol.contains_surface_site()
