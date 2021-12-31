b6de6e223427691a0ec74583506ebe9ccd29fd50
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-12-30 21:12:03 -0500

    fix acsl io, refactor simulated sl for acsl
    - jsonifies sl_kwargs for reading and writing
    - refactors simulated sl so that it now iterates an acsl internally

--- src/autocat/learning/sequential.py
+++ src/autocat/learning/sequential.py
@@ -412,7 +412,17 @@ class AutoCatSequentialLearner:
         # append kwargs for candidate selection
         jsonified_list.append(self.candidate_selection_kwargs)
         # append the acsl kwargs
-        jsonified_list.append(self.sl_kwargs)
+        jsonified_sl_kwargs = {}
+        for k in self.sl_kwargs:
+            if k != "iteration_count" and self.sl_kwargs[k] is not None:
+                jsonified_sl_kwargs[k] = [arr.tolist() for arr in self.sl_kwargs[k]]
+            elif k == "iteration_count":
+                jsonified_sl_kwargs["iteration_count"] = self.sl_kwargs[
+                    "iteration_count"
+                ]
+            elif self.sl_kwargs[k] is None:
+                jsonified_sl_kwargs[k] = None
+        jsonified_list.append(jsonified_sl_kwargs)
 
         if json_name is None:
             json_name = "acsl.json"
@@ -442,7 +452,29 @@ class AutoCatSequentialLearner:
         )
         predictor_kwargs = all_data[-3]
         candidate_selection_kwargs = all_data[-2]
-        sl_kwargs = all_data[-1]
+        raw_sl_kwargs = all_data[-1]
+        sl_kwargs = {}
+        for k in raw_sl_kwargs:
+            if k in [
+                "predictions",
+                "uncertainties",
+                "acquisition_scores",
+                "candidate_indices",
+            ]:
+                sl_kwargs[k] = np.array(raw_sl_kwargs[k])
+            elif k in [
+                "predictions_history",
+                "uncertainties_history",
+                "candidate_index_history",
+            ]:
+                sl_kwargs[k] = [np.array(i) for i in raw_sl_kwargs[k]]
+            elif k == "iteration_count":
+                sl_kwargs[k] = raw_sl_kwargs[k]
+            elif k == "train_idx":
+                sl_kwargs[k] = np.array(raw_sl_kwargs[k], dtype=bool)
+            elif k == "train_idx_history":
+                sl_kwargs[k] = [np.array(i, dtype=bool) for i in raw_sl_kwargs[k]]
+
         return AutoCatSequentialLearner(
             design_space=acds,
             predictor_kwargs=predictor_kwargs,
@@ -515,264 +547,135 @@ def multiple_simulated_sequential_learning_runs(
 
 
 def simulated_sequential_learning(
-    predictor: AutoCatPredictor,
-    design_space: AutoCatDesignSpace,
+    full_design_space: AutoCatDesignSpace,
+    predictor_kwargs: Dict[str, Union[str, float]] = None,
+    candidate_selection_kwargs: Dict[str, Union[str, float]] = None,
     init_training_size: int = 10,
-    testing_structures: List[Atoms] = None,
-    testing_y: np.ndarray = None,
-    acquisition_function: str = "MLI",
-    batch_size_to_add: int = 1,
     number_of_sl_loops: int = None,
-    target_min: float = None,
-    target_max: float = None,
-    include_hhi: bool = False,
-    hhi_type: str = "production",
     write_to_disk: bool = False,
     write_location: str = ".",
-):
+    json_name: str = None,
+) -> AutoCatSequentialLearner:
     """
-    Conducts a simulated sequential learning loop given
-    a data set to explore. Can optionally provide a holdout
-    test set that is never added to the training set
-    to measure transferability at each iteration
+    Conducts a simulated sequential learning loop for a
+    fully labelled design space to explore.
 
     Parameters
     ----------
 
-    predictor:
-        AutoCatPredictor object to be used for fitting and prediction
+    full_design_space:
+        Fully labelled AutoCatDesignSpace to simulate
+        being searched over
 
-    design_space:
-        AutoCatDesignSpace of the design space to be explored.
-        Must have labels for the entire space.
+    predictor_kwargs:
+        Kwargs to be used in setting up the predictor.
+        This is where model class, model hyperparameters, etc.
+        are specified.
+
+    candidate_selection_kwargs:
+        Kwargs that specify that settings for candidate selection.
+        This is where acquisition function, targets, etc. are
+        specified.
 
     init_training_size:
         Size of the initial training set to be selected from
         the full space.
         Default: 10
 
-    testing_structures:
-        List of all Atoms objects to be excluded from the
-        SL search (ie. structures to never be added to the set).
-        Used for testing transferability of the model at each iteration
-
-    testing_y:
-        Labels for corresponding to `testing_structures`
-
-    acquisition_function:
-        Acquisition function to be used to determine candidates to be
-        added to the training set at each iteration of the sl loop
-
-    batch_size_to_add:
-        Number of candidates to be added to the training set on each loop.
-        (ie. N candidates with the highest uncertainties added
-        for each iteration)
-        Default: 1 (ie. adds candidate with max unc on each loop)
-
     number_of_sl_loops:
         Integer specifying the number of sequential learning loops to be conducted.
         This value cannot be greater than
-        `(len(all_training_structures) - init_training_size)/batch_size_to_add`
+        `(DESIGN_SPACE_SIZE - init_training_size)/batch_size_to_add`
         Default: maximum number of sl loops calculated above
 
-    target_min:
-        Label value that ideal candidates should be greater than
-        Default: -inf
-
-    target_max:
-        Label value that ideal candidates should be less than
-        Default: +inf
-
-    include_hhi:
-        Whether HHI scores should be used to weight aq scores
-
-    hhi_type:
-        Type of HHI index to be used for weighting
-        Options
-        - production (default)
-        - reserves
-
     write_to_disk:
-        Boolean specifying whether the sl dictionary should be written to disk as a json.
+        Boolean specifying whether the resulting sequential learner should be
+        written to disk as a json.
         Defaults to False.
 
     write_location:
-        String with the location where sl_dict should be written to disk.
+        String with the location where the resulting sequential learner
+        should be written to disk.
+        Defaults to current directory.
 
     Returns
     -------
 
-    sl_dict:
-        Dictionary containing histories of different quantities throughout
-        the calculation:
-        - training_history: indices that are included in the training set
-        - uncertainty_history: prediction uncertainties at each iteration
-        - predicted_history: all predictions at each iteration
-        - mae_train_history: mae scores on predicting training set
-        - rmse_train_history: rmse scores on predicting training set
-        - max_scores_history: max aq scores for candidate selection
-        - aq_scores_history: all aq scores at each iteration
-        If testing structures and labels given:
-        - mae_test_history: mae scores on testing set
-        - rmse_test_history: rmse scores on testing set
-        - test_prediction_history: predictions on testing set
-        - test_uncertainty_history: uncertainties on predicting on test set
+    sl:
+        Sequential Learner after having been iterated as specified
+        by the input settings. Contains candidate, prediction,
+        and uncertainty histories for further analysis as desired.
     """
 
-    all_training_structures = design_space.design_space_structures
-    all_training_y = design_space.design_space_labels
+    ds_size = len(full_design_space)
 
-    if True in np.isnan(all_training_y):
-        missing_label_idx = np.where(np.isnan(all_training_y))[0]
+    # check fully explored
+    if True in np.isnan(full_design_space.design_space_labels):
+        missing_label_idx = np.where(np.isnan(full_design_space.design_space_labels))[0]
         msg = f"Design space must be fully explored. Missing labels at indices: {missing_label_idx}"
         raise AutoCatSequentialLearningError(msg)
 
-    if init_training_size > len(all_training_structures):
-        msg = f"Initial training size ({init_training_size}) larger than design space ({len(all_training_structures)})"
+    # check that specified initial training size makes sense
+    if init_training_size > ds_size:
+        msg = f"Initial training size ({init_training_size})\
+             larger than design space ({ds_size})"
         raise AutoCatSequentialLearningError(msg)
 
-    max_num_sl_loops = int(
-        np.ceil((len(all_training_structures) - init_training_size) / batch_size_to_add)
-    )
+    batch_size_to_add = candidate_selection_kwargs.get("num_candidates_to_pick", 1)
+    max_num_sl_loops = int(np.ceil((ds_size - init_training_size) / batch_size_to_add))
 
     if number_of_sl_loops is None:
         number_of_sl_loops = max_num_sl_loops
 
+    # check that specified number of loops is feasible
     if number_of_sl_loops > max_num_sl_loops:
         msg = f"Number of SL loops ({number_of_sl_loops}) cannot be greater than ({max_num_sl_loops})"
         raise AutoCatSequentialLearningError(msg)
 
     # generate initial training set
-    train_idx = np.zeros(len(all_training_structures), dtype=bool)
-    train_idx[
-        np.random.choice(
-            len(all_training_structures), init_training_size, replace=False
-        )
-    ] = 1
-    train_history = [train_idx.copy()]
+    init_idx = np.zeros(ds_size, dtype=bool)
+    init_idx[np.random.choice(ds_size, init_training_size, replace=False)] = 1
 
-    # fit on initial training set
-    X = [s for s, i in zip(all_training_structures, train_idx) if i]
-    y = all_training_y[train_idx]
-    predictor.fit(X, y)
+    init_structs = [
+        full_design_space.design_space_structures[idx]
+        for idx, b in enumerate(init_idx)
+        if b
+    ]
+    init_labels = full_design_space.design_space_labels.copy()
+    init_labels = init_labels[np.where(init_idx)]
 
-    pred_history = []
-    unc_history = []
-    max_scores_history = []
-    aq_scores_history = []
-    mae_train_history = []
-    rmse_train_history = []
-    mae_test_history = []
-    rmse_test_history = []
-    test_pred_history = []
-    test_unc_history = []
+    # set up learner that is used for iteration
+    dummy_labels = np.empty(len(full_design_space))
+    dummy_labels[:] = np.nan
+    ds = AutoCatDesignSpace(full_design_space.design_space_structures, dummy_labels)
+    ds.update(init_structs, init_labels)
+    sl = AutoCatSequentialLearner(
+        design_space=ds,
+        predictor_kwargs=predictor_kwargs,
+        candidate_selection_kwargs=candidate_selection_kwargs,
+    )
+    # first iteration on initial dataset
+    sl.iterate()
 
-    def _collect_pred_stats(
-        all_training_structures, all_training_y, testing_structures, testing_y
-    ):
-        _preds, _uncs = predictor.predict(all_training_structures)
-
-        # get scores on full training set
-        mae_train_history.append(
-            predictor.score(all_training_structures, all_training_y)
-        )
-        rmse_train_history.append(
-            predictor.score(
-                all_training_structures, all_training_y, metric="mse", squared=False
-            )
-        )
-
-        # get scores on test perturbations
-        if testing_structures is not None:
-            if testing_y is None:
-                msg = "Labels for the test structures must be provided"
-                raise AutoCatSequentialLearningError(msg)
-
-            mae_test_score, test_preds, test_unc = predictor.score(
-                testing_structures, testing_y, return_predictions=True
-            )
-            mae_test_history.append(mae_test_score)
-            test_pred_history.append([p.tolist() for p in test_preds])
-            test_unc_history.append([u.tolist() for u in test_unc])
-            rmse_test_history.append(
-                predictor.score(
-                    testing_structures, testing_y, metric="mse", squared=False
-                )
-            )
-
-        unc_history.append(_uncs)
-        pred_history.append(_preds)
-        return _preds, _uncs
-
+    # start simulated sequential learning loop
     for i in range(number_of_sl_loops):
         print(f"Sequential Learning Iteration #{i+1}")
-        # make predictions
-        _preds, _uncs = _collect_pred_stats(
-            all_training_structures, all_training_y, testing_structures, testing_y
-        )
-
-        # check that enough data pts left to adhere to batch_size_to_add
-        # otherwise just add the leftover data
-        if batch_size_to_add < len(all_training_y[~train_idx]):
-            bsa = batch_size_to_add
-        else:
-            bsa = len(all_training_y[~train_idx])
-
-        # select next candidate(s)
-        next_candidate_idx, max_scores, aq_scores = choose_next_candidate(
-            labels=all_training_y,
-            train_idx=train_idx,
-            pred=_preds,
-            unc=_uncs,
-            aq=acquisition_function,
-            num_candidates_to_pick=bsa,
-            target_min=target_min,
-            target_max=target_max,
-            include_hhi=include_hhi,
-            hhi_type=hhi_type,
-        )
-        max_scores_history.append([int(i) for i in max_scores])
-        aq_scores_history.append(aq_scores.tolist())
-
-        # add next candidates to training set
-        train_idx[next_candidate_idx] = True
-        train_history.append(train_idx.copy())
-
-        # retrain on training set with new additions
-        X = [s for s, i in zip(all_training_structures, train_idx) if i]
-        y = all_training_y[train_idx]
-        predictor.fit(X, y)
-
-    # make preds on final model
-    _, _ = _collect_pred_stats(
-        all_training_structures, all_training_y, testing_structures, testing_y
-    )
-
-    sl_dict = {
-        "training_history": [th.tolist() for th in train_history],
-        "uncertainty_history": [mu.tolist() for mu in unc_history],
-        "prediction_history": [p.tolist() for p in pred_history],
-        "mae_train_history": mae_train_history,
-        "rmse_train_history": rmse_train_history,
-        "max_scores_history": max_scores_history,
-        "aq_scores_history": aq_scores_history,
-    }
-
-    if testing_structures is not None:
-        sl_dict["mae_test_history"] = mae_test_history
-        sl_dict["rmse_test_history"] = rmse_test_history
-        sl_dict["test_prediction_history"] = test_pred_history
-        sl_dict["test_uncertainty_history"] = test_unc_history
+        if sl.candidate_indices is not None:
+            next_structs = sl.candidate_structures
+            next_labels = full_design_space.design_space_labels.take(
+                sl.candidate_indices
+            )
+            sl.design_space.update(next_structs, next_labels)
+            sl.iterate()
 
     if write_to_disk:
-        if not os.path.isdir(write_location):
-            os.makedirs(write_location)
-        json_write_path = os.path.join(write_location, "sl_dict.json")
-        with open(json_write_path, "w") as f:
-            json.dump(sl_dict, f)
-        print(f"SL dictionary written to {json_write_path}")
+        # for now does not write out model class
+        # serialization needs to be implemented
+        sl.predictor_kwargs.update({"model_class": None})
+        sl.write_json(write_location=write_location, json_name=json_name)
+        print(f"SL dictionary written to {write_location}")
 
-    return sl_dict
+    return sl
 
 
 def choose_next_candidate(
