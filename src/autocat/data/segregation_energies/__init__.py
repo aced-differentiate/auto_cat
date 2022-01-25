4c24ddc745bd8713ed92c8810e55a90857ca6adc
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2022-01-25 17:33:31 -0500

    add Rao 2020 saa stability dataset

--- src/autocat/data/segregation_energies/__init__.py
+++ src/autocat/data/segregation_energies/__init__.py
@@ -1,18 +1,29 @@
 import json
 import pkg_resources
 
-__all__ = ["SEGREGATION_ENERGIES"]
+__all__ = ["RABAN1999_SEGREGATION_ENERGIES", "RAO2020_SEGREGATION_ENERGIES"]
 """
-Values obtained from https://doi.org/10.1103/PhysRevB.59.15990
 
-SEGREGATION_ENERGIES:
+RABAN1999_SEGREGATION_ENERGIES:
+    Values obtained from https://doi.org/10.1103/PhysRevB.59.15990
     Segregation energies for different host/dopant combinations
     For hosts used fcc: 111, bcc:110 (Fe100 also available), hcp:0001
+
+RAO2020_SEGREGATION_ENERGIES:
+    Values obtained from https://doi.org/10.1007/s11244-020-01267-2
+    Segregation energies for different host/dopant combinations
 """
 
-raw_seg_ener = pkg_resources.resource_filename(
-    "autocat.data.segregation_energies", "segregation_energies.json"
+raw_raban_seg_ener = pkg_resources.resource_filename(
+    "autocat.data.segregation_energies", "raban1999.json"
 )
 
-with open(raw_seg_ener) as fr:
-    SEGREGATION_ENERGIES = json.load(fr)
+with open(raw_raban_seg_ener) as fr:
+    RABAN1999_SEGREGATION_ENERGIES = json.load(fr)
+
+raw_rao_seg_ener = pkg_resources.resource_filename(
+    "autocat.data.segregation_energies", "rao2020.json"
+)
+
+with open(raw_rao_seg_ener) as fr:
+    RAO2020_SEGREGATION_ENERGIES = json.load(fr)
