1fcce073430786ff390267b6db31c2e3618b4003
Vinay Hegde <vhegde@citrine.io>
2022-03-17 20:23:12 -0700

    test invalid/alternate input types; add placement tests

--- tests/test_adsorption.py
+++ tests/test_adsorption.py
@@ -1,7 +1,6 @@
 """Unit tests for the `autocat.adsorption` module."""
 
 import os
-import shutil
 import tempfile
 
 import pytest
@@ -12,11 +11,12 @@ from ase.build import molecule
 
 from autocat.saa import generate_saa_structures
 from autocat.surface import generate_surface_structures
-from autocat.adsorption import generate_adsorbed_structures
 from autocat.adsorption import generate_molecule
+from autocat.adsorption import generate_adsorbed_structures
+from autocat.adsorption import place_adsorbate
 from autocat.adsorption import get_adsorbate_height_estimate
 from autocat.adsorption import get_adsorbate_slab_nn_list
-from autocat.adsorption import place_adsorbate
+from autocat.adsorption import get_adsorption_sites
 from autocat.adsorption import AutocatAdsorptionGenerationError
 
 
@@ -52,74 +52,67 @@ def test_generate_molecule_disk_io():
     assert os.path.samefile(mol["traj_file_path"], traj_file_path)
 
 
-def test_place_adsorbate_defaults():
-    pass
+def test_generate_adsorbed_structures_invalid_inputs():
+    surf = generate_surface_structures(species_list=["Fe"])["Fe"]["bcc100"]["structure"]
+    # no surface input
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures()
+    # incorrect surface type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(surface="Fe")
+    # no adsorbates input
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(surface=surf)
+    # incorrect adsorbates type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(surface=surf, adsorbates=surf)
+    # incorrect rotations type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(surface=surf, adsorbates=["H"], rotations="x")
+    # incorrect adsorption_sites type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(
+            surface=surf, adsorbates=["H"], adsorption_sites=[(0, 0)]
+        )
+    # incorrect use_all_sites type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(
+            surface=surf, adsorbates=["H"], use_all_sites=[True, True]
+        )
+    # incorrect site_types type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(
+            surface=surf, adsorbates=["H"], site_types="bridge"
+        )
+    # incorrect heights type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(surface=surf, adsorbates=["H"], heights=[1.5])
+    # incorrect anchor_atom_indices type
+    with raises(AutocatAdsorptionGenerationError):
+        generate_adsorbed_structures(
+            surface=surf, adsorbates=["H"], anchor_atom_indices=[0, 1]
+        )
 
 
-def test_place_adsorbate():
-    pass
-
-
-def test_get_adsorbate_height_estimate():
-    # Tests height estimation based on covalent radii of nn
-    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
-    m = generate_molecule(molecule_name="O")["structure"]
-    # Checks ontop corresponds to cov_rad1 + cov_rad2
-    assert get_adsorbate_height_estimate(
-        surface=surf, adsorbate=m, adsorption_site=(0.0, 0.0)
-    ) == approx(2.02)
-    # Checks hollow placement maintains distance of cov_rad1+cov_rad2
-    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
-    m = generate_molecule(molecule_name="OH")["structure"]
-    # Checks average estimated height based on nn is used
-    assert get_adsorbate_height_estimate(
-        surface=surf, adsorbate=m, adsorption_site=(3.82898322, 0.73688816)
-    ) == approx(1.3222644717)
-
-
-def test_get_adsorbate_slab_nn_list():
-    # Tests generation of nn list for adsorbate placement
-    # Checks number of nn identified
-    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
-    nn_list = get_adsorbate_slab_nn_list(
-        surface=surf, adsorption_site=(3.82898322, 0.73688816)
+def test_generate_adsorbed_structures_valid_input_types():
+    surf = generate_surface_structures(species_list=["Fe"])["Fe"]["bcc100"]["structure"]
+    # single rotations list input
+    ads = generate_adsorbed_structures(
+        surface=surf, adsorbates=["CO2"], rotations=[(90, "x")]
     )
-    assert len(nn_list[0]) == 3
-    # Checks that 3 positions given (1 for each nn)
-    assert len(nn_list[1]) == 3
-    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
-
-    surf = generate_surface_structures(["Fe"])["Fe"]["bcc100"]["structure"]
-    nn_list = get_adsorbate_slab_nn_list(surface=surf, adsorption_site=(0.0, 0.0))
-    # Check that 1 nn identified
-    assert len(nn_list[0]) == 1
-    # Checks that 1 position given (1 for each nn)
-    assert len(nn_list[1]) == 1
-    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
-
-    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
-    nn_list = get_adsorbate_slab_nn_list(
-        surface=surf, adsorption_site=(6.92964646, 4.15778787),
+    assert ads["CO2"]["origin"]["0_0"]["structure"].positions[-1][2] == approx(16.385)
+    # single heights value input
+    ads = generate_adsorbed_structures(surface=surf, adsorbates=["CO2"], heights=3.5)
+    assert ads["CO2"]["origin"]["0_0"]["structure"].positions[-1][2] == approx(
+        16.626342
     )
-    # Checks that 4 nn identified
-    assert len(nn_list[0]) == 4
-    # Checks that 4 position given (1 for each nn)
-    assert len(nn_list[1]) == 4
-    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
-
-    saa = generate_saa_structures(["Cu"], ["Pt"])["Cu"]["Pt"]["fcc111"]["structure"]
-    nn_list = get_adsorbate_slab_nn_list(
-        surface=saa, adsorption_site=(7.01980257, 3.31599674)
+    # single anchor_atom_index input
+    ads = generate_adsorbed_structures(
+        surface=surf, adsorbates=["CO2"], anchor_atom_indices=1
     )
-    # Checks correct nn identified
-    assert nn_list[0][0] == "Pt"
-    assert nn_list[0][1] == "Cu"
-    assert nn_list[1][0][0] == approx(saa[27].x)
-    assert nn_list[1][1][1] == approx(saa[28].y)
-
-
-def test_get_adsorption_sites():
-    pass
+    assert ads["CO2"]["origin"]["0_0"]["structure"].positions[-1][2] == approx(
+        13.927684
+    )
 
 
 def test_generate_adsorbed_structures_data_structure():
@@ -324,3 +317,103 @@ def test_generate_adsorbed_structures_dirs_exist_ok():
     assert os.path.samefile(
         ads["H"]["origin"]["0.0_0.0"]["traj_file_path"], traj_file_path
     )
+
+
+def test_place_adsorbate_defaults():
+    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
+    mol = molecule("NH3")
+    ads = place_adsorbate(surface=surf, adsorbate=mol)
+    # check that the first atom in the molecule (N) is at the origin (default
+    # adsorption_site)
+    assert ads.positions[-4][:2] == approx([0, 0])
+    # check that the molecule is placed on the surface without rotation.
+    # By default NH3 is in the following configuration:  N
+    #                                                  / | \
+    #                                                 H  H  H
+    #                                                 surface
+    assert ads.positions[-1][2] == approx(ads.positions[-2][2])
+    assert ads.positions[-1][0] == approx(-1 * ads.positions[-2][0])
+
+
+def test_get_adsorbate_height_estimate_defaults():
+    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
+    mol = molecule("NH3")
+    default_h = get_adsorbate_height_estimate(surface=surf, adsorbate=mol)
+    origin_h = get_adsorbate_height_estimate(
+        surface=surf, adsorbate=mol, adsorption_site=(0, 0)
+    )
+    assert default_h == approx(origin_h)
+
+
+def test_get_adsorbate_height_estimate():
+    # Tests height estimation based on covalent radii of nn
+    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
+    m = generate_molecule(molecule_name="O")["structure"]
+    # Checks ontop corresponds to cov_rad1 + cov_rad2
+    assert get_adsorbate_height_estimate(
+        surface=surf, adsorbate=m, adsorption_site=(0.0, 0.0)
+    ) == approx(2.02)
+    # Checks hollow placement maintains distance of cov_rad1+cov_rad2
+    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
+    m = generate_molecule(molecule_name="OH")["structure"]
+    # Checks average estimated height based on nn is used
+    assert get_adsorbate_height_estimate(
+        surface=surf, adsorbate=m, adsorption_site=(3.82898322, 0.73688816)
+    ) == approx(1.3222644717)
+
+
+def test_get_adsorbate_slab_nn_list_defaults():
+    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
+    default_nn_list = get_adsorbate_slab_nn_list(surface=surf)
+    origin_nn_list = get_adsorbate_slab_nn_list(surface=surf, adsorption_site=(0, 0))
+    # check that the default xy coordinates of the nn Cu atom is at the origin
+    assert default_nn_list[1][0][0] == origin_nn_list[1][0][0]
+    assert default_nn_list[1][0][1] == origin_nn_list[1][0][1]
+
+
+def test_get_adsorbate_slab_nn_list():
+    # Tests generation of nn list for adsorbate placement
+    # Checks number of nn identified
+    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
+    nn_list = get_adsorbate_slab_nn_list(
+        surface=surf, adsorption_site=(3.82898322, 0.73688816)
+    )
+    assert len(nn_list[0]) == 3
+    # Checks that 3 positions given (1 for each nn)
+    assert len(nn_list[1]) == 3
+    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
+
+    surf = generate_surface_structures(["Fe"])["Fe"]["bcc100"]["structure"]
+    nn_list = get_adsorbate_slab_nn_list(surface=surf, adsorption_site=(0.0, 0.0))
+    # Check that 1 nn identified
+    assert len(nn_list[0]) == 1
+    # Checks that 1 position given (1 for each nn)
+    assert len(nn_list[1]) == 1
+    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
+
+    surf = generate_surface_structures(["Pt"])["Pt"]["fcc100"]["structure"]
+    nn_list = get_adsorbate_slab_nn_list(
+        surface=surf, adsorption_site=(6.92964646, 4.15778787),
+    )
+    # Checks that 4 nn identified
+    assert len(nn_list[0]) == 4
+    # Checks that 4 position given (1 for each nn)
+    assert len(nn_list[1]) == 4
+    assert all([nn_list[1][i].shape == (3,) for i in range(len(nn_list[0]))])
+
+    saa = generate_saa_structures(["Cu"], ["Pt"])["Cu"]["Pt"]["fcc111"]["structure"]
+    nn_list = get_adsorbate_slab_nn_list(
+        surface=saa, adsorption_site=(7.01980257, 3.31599674)
+    )
+    # Checks correct nn identified
+    assert nn_list[0][0] == "Pt"
+    assert nn_list[0][1] == "Cu"
+    assert nn_list[1][0][0] == approx(saa[27].x)
+    assert nn_list[1][1][1] == approx(saa[28].y)
+
+
+def test_get_adsorption_sites_defaults():
+    surf = generate_surface_structures(["Cu"])["Cu"]["fcc111"]["structure"]
+    ads_sites = get_adsorption_sites(surface=surf)
+    default_site_types = ["bridge", "hollow", "ontop"]
+    assert not set(ads_sites.keys()).difference(set(default_site_types))
