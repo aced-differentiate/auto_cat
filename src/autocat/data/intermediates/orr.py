75fa1de7ba61309a71f8f5349d1669ea9bbc5a5c
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2022-07-01 12:30:14 -0400

    rotate oh and nh defaults to 45deg

--- src/autocat/data/intermediates/orr.py
+++ src/autocat/data/intermediates/orr.py
@@ -5,5 +5,5 @@ ORR_INTERMEDIATE_NAMES = ["OOH", "O", "OH"]
 # O is in atomic symbols
 ORR_MOLS = {
     "OOH": Atoms("O2H", [(0.0, 0.0, 0.0), (0.0, 1.2, 0.8), (0.0, 0.8, 1.6)]),
-    "OH": Atoms("OH", [(0.0, 0.0, 0.0), (0.0, 0.0, 0.97)]),
+    "OH": Atoms("OH", [(0.0, 0.0, 0.0), (0.69, 0.0, 0.69)]),
 }
